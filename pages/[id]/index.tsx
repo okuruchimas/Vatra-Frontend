5a3213a80e0d48131e98ae4b7666e1489aa9c28e
Dmytro <lianeso4kas@gmail.com>
2022-10-04 17:40:36 +0300

    added new section to repertoire page

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -18,7 +18,6 @@ const Repertoire = () => {
     setRepertoire(TestArr.find(({ link }) => link === id));
   }, [id]);
 
-  console.log(repertoire, 1);
   if (!repertoire) return;
   return (
     <Wrap>
@@ -27,7 +26,58 @@ const Repertoire = () => {
         type={repertoire.type}
         date={repertoire.date}
       />
-      {repertoire.title}
+      <DetailWrap>
+        <RedTitle> Репертуар</RedTitle>
+        <RepertoireDescription>
+          <p>
+            -Ну ти ж повернешся, Лесю? -Обовязково повернуся, Андрію..! -А коли?
+            -Ох, не знаю..
+          </p>
+          <p>
+            Чому Леся так скоро їде, хоча вони з Андрієм тільки зустрілись?
+            Війна змушує частіше розставатись, але палкіше любити. Діти
+            виражають почуття чесніше, взаємодіють щиріше. Й ще: наша лялькова
+            вистава про це.
+          </p>
+          <p>
+            Маленькі, але сміливі серцем Леся і Андрій хочуть допомогти країні,
+            й тому виконують різні завдання, які трапляються на їхньому шляху.
+            Вони відчувають свої силу, впевненість дужче, коли знаходяться
+            разом.
+          </p>
+          <p>
+            Підтримка, внутрішня радість і бажання допомогти вколисуються у
+            душах наших героїв_їнь. Епізодичних або головних.
+          </p>
+          <p>
+            Це наша добра історія Лесі та Андрія, сповнена пригод, взаємності,
+            щемливості та надії з порошинками чарівності.
+          </p>
+          <p>
+            *У виставі лунає жива музика <br />
+            **Після вистави є можливість придбати її мерч - авторську пєсу
+          </p>
+        </RepertoireDescription>
+      </DetailWrap>
+      <TeamInfo>
+        <RedText>Режисер:</RedText> <WhiteText> Артем Вусик</WhiteText>
+        <RedText>Актори:</RedText>
+        <WhiteText>
+          Андрій — Антон Репях Леся,
+          <br /> бабуся Наталя — Ліза Прасолова Оповідач,
+          <br />
+          Яким, коти, родина - Артур Слісаренко
+        </WhiteText>
+        <RedText>Художники:</RedText>{" "}
+        <WhiteText>
+          Режисер — Артем Вусик <br />
+          Андрій — Антон Репях Леся, <br />
+          бабуся Наталя — Ліза Прасолова <br />
+          Оповідач, Яким, коти, родина - Артур Слісаренко <br />
+          Виготовлення декорацій — Антон Репях, <br />
+        </WhiteText>
+        <DateText>Премєра — 21 травня 2022</DateText>
+      </TeamInfo>
     </Wrap>
   );
 };
@@ -35,6 +85,50 @@ const Repertoire = () => {
 export default Repertoire;
 
 const Wrap = styled.div`
-  padding: 10vh 6vw;
+  padding: 9vh 6vw;
   width: 100vw;
+`;
+const RedTitle = styled.span`
+  text-transform: uppercase;
+  font-family: "namu-1400";
+  font-size: 10vh;
+  color: #b11212;
+`;
+const DetailWrap = styled.div`
+  margin-top: 8vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const RepertoireDescription = styled.div`
+  font-size: 2.4vh;
+  width: 50%;
+  border-bottom: 1.6px solid white;
+  margin-bottom: 4vh;
+`;
+
+const TeamInfo = styled.div`
+  margin-left: auto;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const RedText = styled.span`
+  font-family: "namu-1400";
+  font-size: 3.6vh;
+  color: #b11212;
+  margin-bottom: 2vh;
+`;
+const WhiteText = styled.span`
+  font-family: "namu-pro";
+  font-size: 2.8vh;
+  margin-bottom: 2vh;
+`;
+
+const DateText = styled.span`
+  font-family: "namu-pro";
+  font-size: 3.2vh;
+  margin-top: 2.8vh;
 `;
