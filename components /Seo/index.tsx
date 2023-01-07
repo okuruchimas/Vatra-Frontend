5888b6518f3f9c78195643ad567580b520e1f0a1
Dmytro <lianeso4kas@gmail.com>
2023-01-07 16:48:23 +0200

    added seo

--- components /Seo/index.tsx
+++ components /Seo/index.tsx
@@ -4,7 +4,14 @@ const Seo = () => {
   return (
     <Head>
       <title>Theatre Varta</title>
-      <meta name="description" content="some text with dev" />
+      <meta
+        name="title"
+        content="Театр “Варта” на Сторожі Української Культури, м. Львів."
+      />
+      <meta
+        name="description"
+        content="Купити квитки на виставу,  Український театр у місті Львів, вул. Городоцька, 36. +380990000000"
+      />
       <link rel="icon" href="/icons/favicon.svg" />
     </Head>
   );
